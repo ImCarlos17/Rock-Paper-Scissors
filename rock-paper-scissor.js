ac8594479e150f7748c6923203a4b46b3b69ac78
Carlos Barreto <carlosmba96@gmail.com>
2022-02-23 14:37:20 -0400

    Finished-Project

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -23,35 +23,63 @@ function playRound(playerSelection, computerSelection) {
     return "It Was a Tie!";
   }
 }
+const buttons = document.querySelectorAll("button");
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
+buttons.forEach((button) => {
+  button.addEventListener("click", playgame);
+});
 
-  for (let i = 0; i < 5; i++) {
-    const playerSelection = prompt(
-      "what option will you choose, Rock-Paper-Scissors?"
-    );
-    const computerSelection = computerPlay();
-    if (playRound(playerSelection, computerSelection) == "You Lose") {
-      computerScore++;
-      console.log(
-        `You Lose: Player Score: ${playerScore}, Computer Score: ${computerScore}`
-      );
-    } else if (playRound(playerSelection, computerSelection) == "You Win") {
-      playerScore++;
-      console.log(
-        `You Win: Player Score: ${playerScore}, Computer Score: ${computerScore}`
-      );
-    } else {
-      console.log("It Was a Tie!");
-    }
+const pointCounter = document.querySelector("#counter");
+
+const para1 = document.createElement("p");
+const para2 = document.createElement("p");
+const para3 = document.createElement("p");
+const para4 = document.createElement("p");
+
+let playerScore = 0;
+let computerScore = 0;
+
+function playgame(e) {
+  let result = "";
+
+  const playerSelection = e.target.dataset.value;
+  const computerSelection = computerPlay();
+
+  if (playRound(playerSelection, computerSelection) == "You Lose") {
+    computerScore++;
+    para1.textContent = ` Player Score: ${playerScore}`;
+    para2.textContent = `Computer Score: ${computerScore}`;
+    result = `You Lose`;
+    para3.textContent = result;
+    pointCounter.appendChild(para1);
+    pointCounter.appendChild(para2);
+    pointCounter.appendChild(para3);
+  } else if (playRound(playerSelection, computerSelection) == "You Win") {
+    playerScore++;
+    para1.textContent = ` Player Score: ${playerScore}`;
+    para2.textContent = `Computer Score: ${computerScore}`;
+    result = `You Win`;
+    para3.textContent = result;
+    pointCounter.appendChild(para1);
+    pointCounter.appendChild(para2);
+    pointCounter.appendChild(para3);
+  } else if (playRound(playerSelection, computerSelection) == "It Was a Tie!") {
+    para1.textContent = ` Player Score: ${playerScore}`;
+    para2.textContent = `Computer Score: ${computerScore}`;
+    result = "It Was a Tie!";
+    para3.textContent = result;
+    pointCounter.appendChild(para1);
+    pointCounter.appendChild(para2);
+    pointCounter.appendChild(para3);
   }
-  if (playerScore > computerScore) {
-    alert("You Win");
-  } else if (playerScore < computerScore) {
-    alert("Computer Win");
+
+  if (playerScore == 5) {
+    result = "The winner is Player";
+    para3.textContent = result;
+    pointCounter.appendChild(para3);
+  } else if (computerScore == 5) {
+    result = "The winner is Computer";
+    para3.textContent = result;
+    pointCounter.appendChild(para3);
   }
 }
-
-game();
